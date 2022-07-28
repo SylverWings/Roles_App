97e34c92267dcfdc89910d4d8ad30bd6dc860e41
Lionel <Lionel5garcia@gmail.com>
2022-07-28 13:28:06 +0200

    feat: middleware isSuperAdmin with logic

--- app/Http/Middleware/IsSuperAdmin.php
+++ app/Http/Middleware/IsSuperAdmin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -18,6 +19,22 @@ class IsSuperAdmin
     public function handle(Request $request, Closure $next)
     {
         Log::info("is Super Admin");
+
+        $userId = auth()->user()->id;
+        $user = User::find($userId);
+
+        $hasRole = $user->roles->contains(3);
+
+        if(!$hasRole){
+            return response()->json(
+                [
+                    "success"=> true,
+                    "message"=> "Dont have permissions"
+                ],
+                400
+            );
+        }
+
         return $next($request);
     }
 }
