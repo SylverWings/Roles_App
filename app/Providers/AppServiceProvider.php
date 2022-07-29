96c112db83402666e117249474a97496b159eb75
Lionel <Lionel5garcia@gmail.com>
2022-07-29 13:18:03 +0200

    fix: legnth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
     
     public function boot(UrlGenerator $url)
     {
+        Schema::defaultStringLength(191);
         if(env('REDIRECT_HTTOS')){
             $url->formatScheme('https://');
         }
